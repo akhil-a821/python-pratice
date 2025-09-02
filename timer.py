07e4786690f2d7e94129bf96469b78696b6b7112
akhil-a821 <akuakhila1@gmail.com>
2025-09-02 23:15:09 +0530

    added time up sound and seconds ticking sound

--- timer.py
+++ timer.py
@@ -1,9 +1,19 @@
 import time
-timer=int(input("enter the time in seconds:"))
-for x in range(timer,0,-1):
+import winsound
+while True:
+ timer=int(input("enter the time in seconds:"))
+ for x in range(timer,0,-1):
     seconds=int(x%60)
     minutes=int((x/60)%60)
     hours=int((x/3600)%24)
     print(f"{hours:02}:{minutes:02}:{seconds:02}",end="\r")
     time.sleep(1)
-print("Time's up!")
+    winsound.Beep(1000,100)
+ print("Time's up!")
+ for i in range(5):
+     winsound.Beep(1200,500)
+ a=input("Do you want to set another timer? (yes/no): ")
+ if a.lower()!="yes":
+        break
+ else:
+        continue
