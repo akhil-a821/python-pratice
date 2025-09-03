8c36f57c739131fe02af7c1061072267073e543c
akhil-a821 <akuakhila1@gmail.com>
2025-09-03 13:14:45 +0530

    added some changes

--- shoppingcart.py
+++ shoppingcart.py
@@ -2,14 +2,16 @@ foods=[]
 prices=[]
 total=0
 while True:
- food=input("enter the food you want to buy.(q) to quit:")
+ food=input("enter the food you want to buy.((q) to quit):")
  if food.lower()=="q":
     break
  else:
    food=foods.append(food)
-   price=int(input("enter the price of the food:"))
+   price=int(input("enter the price of the food:$"))
    total=total+float(price) 
    price=prices.append(price)
-   
-print(f"your shopping cart items are:{foods}",end=" ")
-print(f"total price is:${total}")
+print("----your shopping cart----")   
+print(f"items are:{foods}",end=" ")
+print()
+print(f"total price of {len(foods)} item is:${total}")
+print("----thanks for shopping with us----")
